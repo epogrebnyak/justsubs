3470e79142af5b94305cc0f1efa14711a28ab360
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-03-23 20:39:08 +0000

    more comment

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from justsubs import Video
 
-# List subtitle options
+# List subtitle options, find out "en-uYU-mmqFLq8"
 video = Video("KzWS7gJX5Z8")
 video.list_subs()
 
@@ -8,6 +8,6 @@ video.list_subs()
 subs = Video("KzWS7gJX5Z8").subtitles(language="en-uYU-mmqFLq8")
 subs.download()
 
-# Print subtitles
+# Print subtitles as plain text
 print(subs.get_text_blocks()[:10])
 print(subs.get_plain_text()[:550])
