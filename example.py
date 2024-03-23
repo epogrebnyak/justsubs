2a91f22953042dab7a711fdd9db6fdebea728b6c
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-03-23 23:33:43 +0300

    pytest error - justsubs not found

--- example.py
+++ example.py
@@ -5,9 +5,9 @@ video = Video("KzWS7gJX5Z8")
 video.list_subs()
 
 # Download subtitles
-# subs = Video("KzWS7gJX5Z8").subtitles(language="en-uYU-mmqFLq8")
-# subs.download()
+subs = Video("KzWS7gJX5Z8").subtitles(language="en-uYU-mmqFLq8")
+subs.download()
 
 # Print subtitles
-# print(subs.get_text_blocks()[:10])
-# print(subs.get_plain_text()[:550])
+print(subs.get_text_blocks()[:10])
+print(subs.get_plain_text()[:550])
